3474c61ab10848044fb22ae25f0b2d9c1654f09d
Kenneth Newall <kennethsean.newall@arvato.com>
2023-05-12 12:43:46 +0200

    added load screen for session

--- src/app/sessions/feature/session-main/session.component.ts
+++ src/app/sessions/feature/session-main/session.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { BehaviorSubject, Subject, filter, map, merge, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, Subject, combineLatest, filter, map, merge, shareReplay, switchMap, take, tap, zip } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { SessionService } from '../../data-access/session.service';
 import { SessionHostInfo, Session, SessionInfo } from '../../models/session';
@@ -14,8 +14,11 @@ export class SessionComponent {
   constructor(private _sessionService: SessionService, private _route: ActivatedRoute) {
   }
 
-  session = new Subject<Session>();
-  messages = new BehaviorSubject<string[]>([]);
+  private session = new Subject<Session>();
+  private messages = new BehaviorSubject<string[]>([]);
+  private load = new Subject<boolean>();
+
+  messages$ = this.messages.pipe();
 
   getSessionById$ = this._route.params.pipe(
     map(p => p['sessionId']),
@@ -30,10 +33,14 @@ export class SessionComponent {
 
   currentSession$ = merge(this.createSession$, this.getSessionById$).pipe(
     filter(session => session !== undefined),
-    tap(session => this._sessionService.connect(session.id, message => this.messages.next([message, ...this.messages.value])))
+    tap(session => this._sessionService.connect(session.id, message => this.messages.next([message, ...this.messages.value]))),
+    shareReplay(1)
   );
 
+  loading$ = this.load.pipe();
+
   registerSession(sessionName: string): void {
+    this.load.next(true);
     const session = { name: sessionName };
     this.session.next(session);
   }
