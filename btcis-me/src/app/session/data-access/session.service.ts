082777b44cccde968ffda2b45438c4583f0deec8
Kenneth Newall <kennethsean.newall@arvato.com>
2023-05-13 08:43:56 +0200

    migrated services

--- btcis-me/src/app/session/data-access/session.service.ts
+++ btcis-me/src/app/session/data-access/session.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { Observable, map } from 'rxjs';
+import { Session, SessionAction, SessionHostInfo, SessionInfo } from '../models/session';
+import { Message } from '../models/message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
+  constructor(@Inject('BTCIS.ME-API') private url: string, private httpClient: HttpClient) { }
 
-  constructor() { }
+  getSession(sessionId: string): Observable<SessionInfo> {
+    return this.httpClient.get(`${this.url}/v1/sessions/${sessionId}`).pipe(
+      map(value => <SessionInfo>value)
+    )
+  }
+
+  createSession(session: Session): Observable<SessionHostInfo> {
+    return this.httpClient.post(`${this.url}/v1/sessions`, session).pipe(
+      map(value => <SessionHostInfo>value)
+    )
+  }
+
+  sendMessage(session: SessionHostInfo, action: SessionAction, message?: Message): Observable<SessionInfo> {
+    const req = {
+      controlId: session.controlId,
+      action: action,
+      data: message
+    };
+    return this.httpClient.post(`${this.url}/v1/sessions/${session.id}/actions`, req).pipe(
+      map(value => <SessionInfo>value)
+    )
+  }
+
+  connect(sessionId: string, listener: (connection: HubConnection) => void): HubConnection {
+    const connection = new HubConnectionBuilder()
+      .withUrl(`${this.url}/sessions-hub`)
+      .build();
+
+    listener(connection);
+
+    connection.start()
+      .then(() => console.log('connection started'))
+      .catch((err) => console.log('error while establishing signalr connection: ' + err));
+
+    return connection;
+  }
 }
--- btcis-me/src/app/session/data-access/user.service.ts
+++ btcis-me/src/app/session/data-access/user.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { User } from '@ionic/cli';
+import { Observable, map } from 'rxjs';
+import { Message } from '../models/message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(@Inject('BTCIS.ME-API') private url: string, private httpClient: HttpClient) { }
+
+  registerUser(sessionId: string, userName: string): Observable<User> {
+    const user = { name: userName };
+    return this.httpClient.post(`${this.url}/v1/sessions/${sessionId}/users`, user).pipe(
+      map(value => <User>value)
+    )
+  }
+
+  sendUserMessage(sessionId: string, userId: string, message: Message): Observable<User> {
+    const req = {
+      data: message
+    };
+    return this.httpClient.post(`${this.url}/v1/sessions/${sessionId}/users/${userId}/actions`, req).pipe(
+      map(value => <User>value)
+    )
+  }
 }
