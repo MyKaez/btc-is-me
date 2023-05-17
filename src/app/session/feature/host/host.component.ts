a6583abc1dfd92c53ee3bc853f8fa29a93a1fc7a
Kenneth Newall <kennethsean.newall@arvato.com>
2023-05-17 13:41:34 +0200

    bug fix: do not update to "prepare" status automatically

--- src/app/session/feature/host/host.component.ts
+++ src/app/session/feature/host/host.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, } from '@angular/core';
 import { SessionControlInfo, SessionInfo, SessionAction } from '../../models/session';
-import { BehaviorSubject, switchMap } from 'rxjs';
+import { BehaviorSubject, filter, shareReplay, switchMap, tap } from 'rxjs';
 import { SessionService } from '../../data-access/session.service';
 
 @Component({
@@ -12,7 +12,7 @@ export class HostComponent {
 
   @Input("session") session!: SessionInfo;
 
-  private sessionStatus = new BehaviorSubject<{ action?: SessionAction, configuration: any }>({ configuration: {} });
+  private sessionStatus = new BehaviorSubject<{ action?: SessionAction, configuration: any }>({ configuration: this.session?.configuration });
 
   constructor(private sessionService: SessionService) {
   }
@@ -22,34 +22,37 @@ export class HostComponent {
   }
 
   sessionUpdates$ = this.sessionStatus.pipe(
+    // if status is "notStarted", we will automatically update the action to "prepare" if we do not filter here!
+    filter(_ => this.session.status !== 'notStarted'),
     switchMap(input => this.sessionService.executeAction(this.controlSession, input.action!, input.configuration))
   );
 
   prepare() {
-    const session = this.createSession('prepare', { simulationType: 'proofOfWork' });
+    const session = this.createUpdate('prepare', { simulationType: 'proofOfWork' });
     this.sessionStatus.next(session);
   }
 
   start() {
-    const session = this.createSession('start');
+    const session = this.createUpdate('start');
     this.sessionStatus.next(session);
   }
 
   stop() {
-    const session = this.createSession('stop');
+    const session = this.createUpdate('stop');
     this.sessionStatus.next(session);
   }
 
-  clear() {
-    const session = this.createSession('reset');
-    this.sessionStatus.next(session);
-  }
-
-  createSession(action: SessionAction, config?: any) {
-    return {
+  createUpdate(action: SessionAction, config?: any) {
+    const update = {
       ...this.sessionStatus.value, action: action, configuration: {
         ...this.sessionStatus.value.configuration, ...(config ?? {})
       }
     };
+    return update;
+  }
+
+  clear() {
+    const update = { action: <SessionAction>'reset', configuration: {} };
+    this.sessionStatus.next(update);
   }
 }
