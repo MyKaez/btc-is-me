a7c2958999ba1fa7a950259b76455a02466d6566
Kenneth Newall <kennethsean.newall@arvato.com>
2023-05-27 10:09:34 +0200

    adjusted host actions

--- src/app/session/feature/host/host.component.ts
+++ src/app/session/feature/host/host.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, ChangeDetectorRef, Component, Input, } from '@angular/core';
-import { SessionControlInfo, SessionInfo, SessionAction } from '../../models/session';
+import { AfterViewInit, ChangeDetectorRef, Component, Input, ViewChild, } from '@angular/core';
+import { SessionControlInfo, SessionInfo, SessionAction, SessionStatus } from '../../models/session';
 import { BehaviorSubject, combineLatest, filter, map, switchMap, withLatestFrom } from 'rxjs';
 import { SessionService } from '../../data-access/session.service';
 import { IonButton } from '@ionic/angular';
@@ -9,24 +9,37 @@ import { IonButton } from '@ionic/angular';
   templateUrl: './host.component.html',
   styleUrls: ['./host.component.scss'],
 })
-export class HostComponent {
+export class HostComponent implements AfterViewInit {
 
   @Input("session") session!: SessionInfo;
+  @ViewChild("prepareButton") prepareButton!: IonButton;
+  @ViewChild("startButton") startButton!: IonButton;
+  @ViewChild("stopButton") stopButton!: IonButton;
+  @ViewChild("clearButton") clearButton !: IonButton;
 
-  constructor(private sessionService: SessionService, private cdRef: ChangeDetectorRef) {
+  private context: { button: IonButton, action: SessionAction, status: SessionStatus }[] = [];
+
+  constructor(private sessionService: SessionService) {
+  }
+
+  ngAfterViewInit(): void {
+    this.context.push({ button: this.prepareButton, action: 'prepare', status: 'notStarted' });
+    this.context.push({ button: this.startButton, action: 'start', status: 'preparing' });
+    this.context.push({ button: this.stopButton, action: 'stop', status: 'started' });
+    this.context.push({ button: this.clearButton, action: 'reset', status: 'stopped' });
+
+    this.context.forEach(button => {
+      if (button.status === this.session.status) {
+        button.button.color = 'primary';
+      } else {
+        button.button.color = 'secondary';
+        button.button.disabled = true;
+      }
+    });
   }
 
   get controlSession(): SessionControlInfo {
     return <SessionControlInfo>this.session;
-  }
-
-  getColor(button: IonButton) {
-    const color = button.color;
-    const newColor = button.disabled ? 'secondary' : 'primary';
-    if (color === newColor)
-      return;
-    button.color = newColor
-    this.cdRef.detectChanges();
   }
 
   prepare() {
@@ -38,11 +51,11 @@ export class HostComponent {
   }
 
   stop() {
-    const session = this.createUpdate('stop');
+    this.createUpdate('stop');
   }
 
   clear() {
-    const update = this.createUpdate('reset');
+    this.createUpdate('reset');
   }
 
   createUpdate(action: SessionAction, config?: any): void {
@@ -54,6 +67,14 @@ export class HostComponent {
         ...(config ?? {})
       }
     };
+    const currentButtonIndex = this.context.findIndex(b => b.action === action)!;
+    const currentButton = this.context[currentButtonIndex];
+    currentButton.button.disabled = true;
+    currentButton.button.color = 'secondary';
+    const nextButton = this.context.length - 1 == currentButtonIndex
+      ? this.context[0].button : this.context[currentButtonIndex + 1].button;
+    nextButton.disabled = false;
+    nextButton.color = 'primary';
     console.log(JSON.stringify(update));
     const subscription = this.sessionService.executeAction(this.controlSession, action, config).subscribe(_ => {
       subscription.unsubscribe();
